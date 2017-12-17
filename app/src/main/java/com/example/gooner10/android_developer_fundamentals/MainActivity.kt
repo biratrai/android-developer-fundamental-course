759c02bd062fedd32bd05f0f9f522105f14b8be2
BIratkirat <Gooner10@Birats-MacBook-Pro.local>
2017-12-17 18:28:32 -0500

    changing package name, adding showToast, countUp method in mainactivity

--- app/src/main/java/com/example/gooner10/android_developer_fundamentals/MainActivity.kt
+++ app/src/main/java/com/example/gooner10/android_developer_fundamentals/MainActivity.kt
@@ -1,12 +1,22 @@
-package com.example.gooner10.android_developer_fundamentals
+package com.example.gooner10.androiddeveloperfundamentals
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.gooner10.android_developer_fundamentals.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun showToast(view: View) {
+
+    }
+
+    fun countUp(view: View) {
+
     }
 }
