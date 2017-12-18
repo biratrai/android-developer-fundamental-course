ab32842541a09d135096e4fa6e24f78317154083
BIratkirat <Gooner10@Birats-MacBook-Pro.local>
2017-12-17 20:35:42 -0500

    passing result back to mainactivyt

--- app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/MainActivity.kt
+++ app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.gooner10.androiddeveloperfundamentals
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+
+const val TEXT_REQUEST = 1
+private val TAG = MainActivity::class.java.simpleName
 
 class MainActivity : AppCompatActivity() {
     private var count = 0
@@ -20,7 +25,7 @@ class MainActivity : AppCompatActivity() {
     fun showToast(view: View) {
         Toast.makeText(this, getString(R.string.toast_message), Toast.LENGTH_SHORT).show()
         val intent = Intent(this, ScrollActivity::class.java)
-        startActivity(intent)
+        startActivityForResult(intent, TEXT_REQUEST)
     }
 
     /**
@@ -28,5 +33,18 @@ class MainActivity : AppCompatActivity() {
      */
     fun countUp(view: View) {
         countTextView.text = Integer.toString(count++)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Log.d(TAG, "requestCode " + requestCode)
+        Log.d(TAG, "resultCode " + resultCode)
+        Log.d(TAG, "intent " + data)
+        if (requestCode == TEXT_REQUEST && resultCode == Activity.RESULT_OK) {
+            if (data != null) {
+                Log.d(TAG, "intent " + data.getStringExtra(REPLY))
+                toastButton.text = data.getStringExtra(REPLY)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/ScrollActivity.kt
+++ app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/ScrollActivity.kt
@@ -1,12 +1,42 @@
 package com.example.gooner10.androiddeveloperfundamentals
 
+import android.app.Activity
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+
+const val REPLY = "Reply"
+private val TAG = ScrollActivity::class.java.simpleName
 
 class ScrollActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scroll)
     }
+
+    override fun onBackPressed() {
+        Log.d(TAG, "onBackPressed")
+        val intent = Intent()
+        intent.putExtra(REPLY, "From onBackPressed")
+        setResult(Activity.RESULT_OK, intent)
+        super.onBackPressed()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        if (item != null) {
+            Log.d(TAG, "" + item.itemId)
+            Log.d(TAG, "onNavigateUp")
+            val intent = Intent().apply {
+                putExtra(REPLY, "From onOptionsItemSelected")
+            }
+            setResult(Activity.RESULT_OK, intent)
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
