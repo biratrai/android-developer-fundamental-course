924abf9dab841734e58a7ed12066deb26140f3eb
BIratkirat <Gooner10@Birats-MacBook-Pro.local>
2017-12-19 18:01:54 -0500

    upgrading gradle, converting test to kotlin

---
+++ app/src/androidTest/java/com/example/gooner10/androiddeveloperfundamentals/MainActivityTest.kt
@@ -0,0 +1,66 @@
+package com.example.gooner10.androiddeveloperfundamentals
+
+
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import android.test.suitebuilder.annotation.LargeTest
+import android.view.View
+import android.view.ViewGroup
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @Rule
+    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun mainActivityTest() {
+        val appCompatButton = onView(
+                allOf(withId(R.id.launchKeyboardActivity), withText("Keyboard Activity"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()))
+        appCompatButton.perform(click())
+
+        val appCompatButton2 = onView(
+                allOf(withId(R.id.showPhone), withText("show Phone"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
+                        isDisplayed()))
+        appCompatButton2.perform(click())
+
+    }
+
+    private fun childAtPosition(
+            parentMatcher: Matcher<View>, position: Int): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return (parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position))
+            }
+        }
+    }
+}
