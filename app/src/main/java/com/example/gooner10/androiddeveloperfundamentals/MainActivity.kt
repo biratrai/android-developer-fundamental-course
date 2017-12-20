b57944a43f70340b1fd5a9cd7092890ca09b3621
Birat Rai <biratrai@hotmail.com>
2017-12-20 15:59:37 -0500

    launching AsyncLoaderActivity

--- app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/MainActivity.kt
+++ app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import com.example.gooner10.androiddeveloperfundamentals.backgroundTask.AsyncLoaderActivity
 
 class MainActivity : AppCompatActivity() {
 
