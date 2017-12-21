0445cee5715aee0c21c47f8bbce55cb681f56e75
Birat Rai <biratrai@hotmail.com>
2017-12-21 18:49:03 -0500

    adding db open helper

--- app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/database/DatabaseActivity.kt
+++ app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/database/DatabaseActivity.kt
@@ -6,8 +6,11 @@ import com.example.gooner10.androiddeveloperfundamentals.R
 
 class DatabaseActivity : AppCompatActivity() {
 
+    private var db: WordListOpenHelper? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_database)
+
+        db = WordListOpenHelper(this)
     }
 }
---
+++ app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/database/WordListOpenHelper.kt
@@ -0,0 +1,71 @@
+package com.example.gooner10.androiddeveloperfundamentals.database
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
+
+/**
+ * Database OpenHelper
+ */
+class WordListOpenHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL(WORD_LIST_TABLE_CREATE)
+        fillDatabaseWithData(db)
+    }
+
+    /**
+     * Adds the initial data set to the database.
+     * According to the docs, onCreate for the open helper does not run on the UI thread.
+     *
+     * @param db Database to fill with data since the member variables are not initialized yet.
+     */
+    private fun fillDatabaseWithData(db: SQLiteDatabase) {
+
+        val words = arrayOf("Android", "Adapter", "ListView", "AsyncTask", "Android Studio", "SQLiteDatabase", "SQLOpenHelper", "Data model", "ViewHolder", "Android Performance", "OnClickListener")
+
+        // Create a container for the data.
+        val values = ContentValues()
+
+        for (i in words.indices) {
+            // Put column/value pairs for current row into the container.
+            values.put(KEY_WORD, words[i]) // put() overrides existing values.
+            // Insert the row.
+            db.insert(WORD_LIST_TABLE, null, values)
+        }
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        Log.w(WordListOpenHelper::class.java.name,
+                "Upgrading database from version " + oldVersion + " to "
+                        + newVersion + ", which will destroy all old data")
+        db.execSQL("DROP TABLE IF EXISTS " + WORD_LIST_TABLE)
+        onCreate(db)
+    }
+
+    companion object {
+
+        private val TAG = WordListOpenHelper::class.java.simpleName
+
+        // Declaring all these as constants makes code a lot more readable and looking like SQL.
+
+        // Version has to be 1 first time or app will crash.
+        private val DATABASE_VERSION = 1
+        private val WORD_LIST_TABLE = "word_entries"
+        private val DATABASE_NAME = "wordlist"
+
+        // Column names...
+        val KEY_ID = "_id"
+        val KEY_WORD = "word"
+
+        // ... and a string array of columns.
+        private val COLUMNS = arrayOf(KEY_ID, KEY_WORD)
+
+        // Build the SQL query that creates the table.
+        private val WORD_LIST_TABLE_CREATE = "CREATE TABLE " + WORD_LIST_TABLE + " (" +
+                KEY_ID + " INTEGER PRIMARY KEY, " + // will auto-increment if no value passed
+
+                KEY_WORD + " TEXT );"
+    }
+}
