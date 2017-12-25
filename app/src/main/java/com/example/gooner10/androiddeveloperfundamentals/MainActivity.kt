fd3690c2e2ba15e850b4236641bbd2c01225eede
Birat Rai <biratrai@hotmail.com>
2017-12-24 23:21:14 -0500

    adding button to launch alarm actiivty

--- app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/MainActivity.kt
+++ app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import com.example.gooner10.androiddeveloperfundamentals.alarm.AlarmActivity
 import com.example.gooner10.androiddeveloperfundamentals.backgroundTask.AsyncLoaderActivity
 import com.example.gooner10.androiddeveloperfundamentals.database.DatabaseActivity
 import com.example.gooner10.androiddeveloperfundamentals.rxKotlin.RxActivity
@@ -37,5 +38,9 @@ class MainActivity : AppCompatActivity() {
 
     fun launchDatabaseActivity(view: View) {
         startActivity(Intent(this, DatabaseActivity::class.java))
+    }
+
+    fun launchAlarmActivity(view: View) {
+        startActivity(Intent(this, AlarmActivity::class.java))
     }
 }
