95ca1d025a8c4587b3a1a8f3c87fea641072648f
Birat Rai <biratrai@hotmail.com>
2017-12-24 23:08:52 -0500

    adding receiver for alarm

---
+++ app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/alarm/AlarmReceiver.kt
@@ -0,0 +1,14 @@
+package com.example.gooner10.androiddeveloperfundamentals.alarm
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class AlarmReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        throw UnsupportedOperationException("Not yet implemented")
+    }
+}
