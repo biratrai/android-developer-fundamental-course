cae50769a24dab63b55ec3ff343c1660944613d5
Birat Rai <biratrai@hotmail.com>
2017-12-24 23:52:39 -0500

    receiving alarm in receiver

--- app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/alarm/AlarmActivity.kt
+++ app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/alarm/AlarmActivity.kt
@@ -1,13 +1,42 @@
 package com.example.gooner10.androiddeveloperfundamentals.alarm
 
-import android.support.v7.app.AppCompatActivity
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.os.SystemClock
+import android.support.v7.app.AppCompatActivity
+import android.view.View
 import com.example.gooner10.androiddeveloperfundamentals.R
 
 class AlarmActivity : AppCompatActivity() {
+    private val ALARM_REQUEST_CODE = 2017
+
+    companion object {
+        const val ALARM_DATA = "data"
+        const val ACTION_USER_ALARM = "com.example.gooner10.USER_ALARM"
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alarm)
+    }
+
+    fun startAlarmManager(view: View) {
+        val intent = Intent(this, AlarmReceiver::class.java)
+        intent.setAction(ACTION_USER_ALARM)
+        intent.putExtra(ALARM_DATA, "From AlarmActivity")
+
+        val pendingIntent: PendingIntent = PendingIntent.getBroadcast(this, ALARM_REQUEST_CODE, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        var alarmManager: AlarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+
+        var currentTimeInMilliSeconds = SystemClock.elapsedRealtime()
+        var ONE_HOUR = 6 * 1000
+
+        var alarmTime = currentTimeInMilliSeconds + ONE_HOUR
+        alarmManager.set(AlarmManager.ELAPSED_REALTIME, alarmTime, pendingIntent)
     }
 }
--- app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/alarm/AlarmReceiver.kt
+++ app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/alarm/AlarmReceiver.kt
@@ -4,15 +4,16 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import com.example.gooner10.androiddeveloperfundamentals.alarm.AlarmActivity.Companion.ACTION_USER_ALARM
+import com.example.gooner10.androiddeveloperfundamentals.alarm.AlarmActivity.Companion.ALARM_DATA
 
 class AlarmReceiver : BroadcastReceiver() {
 
-    val ACTION_USER_ALARM = "com.example.gooner10.USER_ALARM"
     val TAG = AlarmReceiver::class.java.simpleName
 
     override fun onReceive(context: Context, intent: Intent) {
         if (ACTION_USER_ALARM.equals(intent.action)) {
-            Log.d(TAG, "Received intent")
+            Log.d(TAG, "Received intent" + intent.getStringExtra(ALARM_DATA))
         }
     }
 }
