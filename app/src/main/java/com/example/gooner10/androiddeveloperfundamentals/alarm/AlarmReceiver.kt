fc1ff6c3b8c0d57e956cbd8102a20e5229229298
Birat Rai <biratrai@hotmail.com>
2017-12-24 23:14:28 -0500

    adding custom intent-filter and action

--- app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/alarm/AlarmReceiver.kt
+++ app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/alarm/AlarmReceiver.kt
@@ -3,12 +3,16 @@ package com.example.gooner10.androiddeveloperfundamentals.alarm
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 
 class AlarmReceiver : BroadcastReceiver() {
 
+    val ACTION_USER_ALARM = "com.example.gooner10.USER_ALARM"
+    val TAG = AlarmReceiver::class.java.simpleName
+
     override fun onReceive(context: Context, intent: Intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
-        throw UnsupportedOperationException("Not yet implemented")
+        if (ACTION_USER_ALARM.equals(intent.action)) {
+            Log.d(TAG, "Received intent")
+        }
     }
 }
