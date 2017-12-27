905aad4a28d4cd0effd84fc863d3c6a9ac533a0b
Birat Rai <biratrai@hotmail.com>
2017-12-26 21:18:01 -0500

    adding provider activity

---
+++ app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/contentProvider/ContentProviderActivity.kt
@@ -0,0 +1,13 @@
+package com.example.gooner10.androiddeveloperfundamentals.contentProvider
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gooner10.androiddeveloperfundamentals.R
+
+class ContentProviderActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_content_provider)
+    }
+}
