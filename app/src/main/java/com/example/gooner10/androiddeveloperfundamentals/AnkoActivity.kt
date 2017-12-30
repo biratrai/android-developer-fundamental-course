b7398ac5472b0a1057611a788f3091e7b7d7ece1
Birat Rai <biratrai@hotmail.com>
2017-12-30 15:27:42 -0500

    adding anko layout

--- app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/AnkoActivity.kt
+++ app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/AnkoActivity.kt
@@ -2,11 +2,23 @@ package com.example.gooner10.androiddeveloperfundamentals
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.button
+import org.jetbrains.anko.editText
+import org.jetbrains.anko.sdk25.coroutines.onClick
+import org.jetbrains.anko.toast
+import org.jetbrains.anko.verticalLayout
 
 class AnkoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_anko)
+//        setContentView(R.layout.activity_anko)
+
+        verticalLayout {
+            val name = editText()
+            button("Say Hello") {
+                onClick { toast("Hello, ${name.text}!") }
+            }
+        }
     }
 }
