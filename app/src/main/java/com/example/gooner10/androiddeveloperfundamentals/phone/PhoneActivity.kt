ec6a09b3266cca3222a826f1edc527b2910c43a0
Birat Rai <biratrai@hotmail.com>
2017-12-31 11:27:48 -0500

    adding phone activity

---
+++ app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/phone/PhoneActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gooner10.androiddeveloperfundamentals.phone
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PhoneActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_phone)
+    }
+}
