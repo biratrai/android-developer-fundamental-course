f1785dac510d075dfd7dec2b7297c36db081bd5f
Birat Rai <biratrai@hotmail.com>
2018-01-01 11:44:31 -0500

    adding methods to appWidgetProvider

--- app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/widget/WidgetProvider.kt
+++ app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/widget/WidgetProvider.kt
@@ -3,8 +3,13 @@ package com.example.gooner10.androiddeveloperfundamentals.widget
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.util.Log
+import android.widget.RemoteViews
+import com.example.gooner10.androiddeveloperfundamentals.R
 
 class WidgetProvider : AppWidgetProvider() {
+    val TAG = WidgetProvider::class.java.simpleName
+
     override fun onUpdate(context: Context?, appWidgetManager: AppWidgetManager?, appWidgetIds: IntArray?) {
         for (appWidgetId in appWidgetIds!!) {
             updateAppWidget(context, appWidgetManager, appWidgetId)
@@ -12,6 +17,17 @@ class WidgetProvider : AppWidgetProvider() {
     }
 
     private fun updateAppWidget(context: Context?, appWidgetManager: AppWidgetManager?, appWidgetId: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+        // Construct the RemoteViews object.
+        val views = RemoteViews(context!!.packageName,
+                R.layout.app_widget)
+//        views.setTextViewText(R.id.appwidget_update,
+//                context.getResources().getString(
+//                        R.string.date_count_format, 1, dateString))
+        Log.d(TAG, "appWidgetManager: " + appWidgetManager)
+        Log.d(TAG, "view: " + views)
+        Log.d(TAG, "appID: " + appWidgetId)
+        // Instruct the widget manager to update the widget.
+        appWidgetManager!!.updateAppWidget(appWidgetId, views)
     }
 }
