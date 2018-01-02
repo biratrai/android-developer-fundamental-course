cd249bd12c6288850e908ee856e0b2bfda74924f
Birat Rai <biratrai@hotmail.com>
2018-01-02 08:44:20 -0500

    successfully registering for sensor data and receving sensor changed

--- app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/sensor/SensorActivity.kt
+++ app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/sensor/SensorActivity.kt
@@ -2,6 +2,8 @@ package com.example.gooner10.androiddeveloperfundamentals.sensor
 
 import android.content.Context
 import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
@@ -9,9 +11,10 @@ import android.util.Log
 import com.example.gooner10.androiddeveloperfundamentals.R
 import kotlinx.android.synthetic.main.activity_sensor.*
 
-class SensorActivity : AppCompatActivity() {
+class SensorActivity : AppCompatActivity(), SensorEventListener {
     private var sensorManager: SensorManager? = null
     private val TAG = SensorActivity::class.java.simpleName
+    private var sensorLight: Sensor? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,11 +25,38 @@ class SensorActivity : AppCompatActivity() {
         val sensorList = sensorManager!!.getSensorList(Sensor.TYPE_ALL)
         var sensorText = ""
         for (currentSenor in sensorList) {
-            sensorText += "${currentSenor.name} $\n"
-//            sensorText.plus("\n")
+            sensorText += "${currentSenor.name} \n"
 
         }
         Log.d(TAG, "currentSenor: " + sensorText)
         sensor_list.text = sensorText
+
+        sensorLight = sensorManager!!.getDefaultSensor(Sensor.TYPE_LIGHT)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        sensorManager?.registerListener(this, sensorLight, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        sensorManager?.unregisterListener(this)
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        Log.d(TAG, "sensor: " + sensor + accuracy)
+
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        Log.d(TAG, "event: " + event)
+        when (event!!.sensor.type) {
+            Sensor.TYPE_LIGHT -> Log.d(TAG, "TYPE_LIGHT")
+
+            else -> Log.d(TAG, "Not useful sensor")
+
+        }
     }
 }
