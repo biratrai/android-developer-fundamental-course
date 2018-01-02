7a54c029ea8f8b94b5c3476cf6f1673b24dfa1a9
Birat Rai <biratrai@hotmail.com>
2018-01-02 08:32:33 -0500

    getting a list of sensor and displaying it

--- app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/sensor/SensorActivity.kt
+++ app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/sensor/SensorActivity.kt
@@ -1,13 +1,32 @@
 package com.example.gooner10.androiddeveloperfundamentals.sensor
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import com.example.gooner10.androiddeveloperfundamentals.R
+import kotlinx.android.synthetic.main.activity_sensor.*
 
 class SensorActivity : AppCompatActivity() {
+    private var sensorManager: SensorManager? = null
+    private val TAG = SensorActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sensor)
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager?
+
+        val sensorList = sensorManager!!.getSensorList(Sensor.TYPE_ALL)
+        var sensorText = ""
+        for (currentSenor in sensorList) {
+            sensorText += "${currentSenor.name} $\n"
+//            sensorText.plus("\n")
+
+        }
+        Log.d(TAG, "currentSenor: " + sensorText)
+        sensor_list.text = sensorText
     }
 }
