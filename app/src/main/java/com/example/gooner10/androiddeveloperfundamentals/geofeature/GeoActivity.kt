9d87d976857df77de067e3777683d5c7164e9672
Birat Rai <biratrai@hotmail.com>
2018-01-03 10:04:01 -0500

    adding getLastuerlocation function and loggin it

--- app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/geofeature/GeoActivity.kt
+++ app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/geofeature/GeoActivity.kt
@@ -9,22 +9,27 @@ import android.util.Log
 import android.view.View
 import android.widget.Toast
 import com.example.gooner10.androiddeveloperfundamentals.R
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class GeoActivity : AppCompatActivity() {
     private val REQUEST_LOCATION_PERMISSION: Int = 1001
     private val TAG = GeoActivity::class.java.simpleName
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_geo)
     }
 
+    @SuppressWarnings
     fun getLocation(view: View) {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_LOCATION_PERMISSION)
         } else {
             Log.d(TAG, "getLocation: permission granted")
+            getLastUserLocation()
         }
     }
 
@@ -34,9 +39,26 @@ class GeoActivity : AppCompatActivity() {
             REQUEST_LOCATION_PERMISSION -> {
                 if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     Toast.makeText(this, "Permission was granted.", Toast.LENGTH_SHORT).show()
+                    getLastUserLocation()
                 }
             }
             else -> Toast.makeText(this, "Permission was denied.", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    private fun getLastUserLocation() {
+        val fusedLocationProvideClient: FusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            fusedLocationProvideClient.lastLocation.addOnSuccessListener { location ->
+                if (location != null) {
+                    Log.d(Geo.TAG, "onSuccess: ")
+                } else {
+                    Log.d("TAG", "No location")
+                }
+            }
+        }
+
+
     }
 }
