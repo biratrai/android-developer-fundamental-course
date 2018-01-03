f85c90b5d20dd09bf1558b4a926ddd1a59b2cabd
Birat Rai <biratrai@hotmail.com>
2018-01-03 08:32:59 -0500

    adding geo activity

---
+++ app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/geofeature/GeoActivity.kt
@@ -0,0 +1,13 @@
+package com.example.gooner10.androiddeveloperfundamentals.geofeature
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gooner10.androiddeveloperfundamentals.R
+
+class GeoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_geo)
+    }
+}
