69351008045a326601b8cf2b1706d4b13080d131
Birat Rai <biratrai@hotmail.com>
2018-01-04 08:35:52 -0500

    adding places activity

---
+++ app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/places/PlacesActivity.kt
@@ -0,0 +1,13 @@
+package com.example.gooner10.androiddeveloperfundamentals.places
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gooner10.androiddeveloperfundamentals.R
+
+class PlacesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_places)
+    }
+}
