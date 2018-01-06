ea636ce73112da58dbc18f5f2bc3a7d07e5f9c04
Birat Rai <biratrai@hotmail.com>
2018-01-06 09:16:21 -0500

    adding initialization of the class variable

--- app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/customview/FanControllerView.kt
+++ app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/customview/FanControllerView.kt
@@ -1,13 +1,26 @@
 package com.example.gooner10.androiddeveloperfundamentals.customview
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
-import android.widget.ImageView
+import android.view.View
 
 /**
  * Custom ImageView class that shows rotation of a fan
  */
-class FanControllerView : ImageView {
+class FanControllerView : View {
+    private val SELECTION_COUNT = 4 // Total number of selections.
+    private val width: Float = 0.toFloat()                   // Custom view width.
+    private val height: Float = 0.toFloat()                  // Custom view height.
+    private var textPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)               // For text in the view.
+    private var dialPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)               // For dial circle in the view.
+    private val radius: Float = 0.toFloat()                  // Radius of the circle.
+    private var activeSelection: Int = 0           // The active selection.
+    // String buffer for dial labels and float for ComputeXY result.
+    private val tempLabel = StringBuffer(8)
+    private val tempResult = FloatArray(2)
+
     constructor(context: Context) : super(context) {
         init()
     }
@@ -21,6 +34,13 @@ class FanControllerView : ImageView {
     }
 
     private fun init() {
-
+        textPaint.color = Color.BLACK
+        textPaint.style = Paint.Style.FILL_AND_STROKE
+        textPaint.textAlign = Paint.Align.CENTER
+        textPaint.textSize = 40f
+        dialPaint.color = Color.GRAY
+        // Initialize current selection.
+        activeSelection = 0
+        // TODO: Set up onClick listener for this view.
     }
 }
