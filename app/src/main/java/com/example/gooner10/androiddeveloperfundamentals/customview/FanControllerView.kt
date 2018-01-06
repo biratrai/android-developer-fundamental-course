2882477d06f8d9b99e002599cb26219a67314a33
Birat Rai <biratrai@hotmail.com>
2018-01-06 10:03:17 -0500

    adding onSizeChanged and onDraw to add logic to handle change in width, height or radius and drawing text and circle

--- app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/customview/FanControllerView.kt
+++ app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/customview/FanControllerView.kt
@@ -1,10 +1,10 @@
 package com.example.gooner10.androiddeveloperfundamentals.customview
 
 import android.content.Context
+import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
-import android.view.MotionEvent
 import android.view.View
 
 /**
@@ -12,11 +12,11 @@ import android.view.View
  */
 class FanControllerView : View {
     private val SELECTION_COUNT = 4 // Total number of selections.
-    private val width: Float = 0.toFloat()                   // Custom view width.
-    private val height: Float = 0.toFloat()                  // Custom view height.
+    private var width: Float = 0.toFloat()                   // Custom view width.
+    private var height: Float = 0.toFloat()                  // Custom view height.
     private var textPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)               // For text in the view.
     private var dialPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)               // For dial circle in the view.
-    private val radius: Float = 0.toFloat()                  // Radius of the circle.
+    private var radius: Float = 0.toFloat()                  // Radius of the circle.
     private var activeSelection: Int = 0           // The active selection.
     // String buffer for dial labels and float for ComputeXY result.
     private val tempLabel = StringBuffer(8)
@@ -43,10 +43,45 @@ class FanControllerView : View {
         // Initialize current selection.
         activeSelection = 0
         // TODO: Set up onClick listener for this view.
-        setOnTouchListener(object : View.OnTouchListener {
-            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
-                return false
-            }
-        })
+        setOnClickListener { v -> v.isClickable }
+    }
+
+    private fun computeXYForPosition(pos: Int, radius: Float): FloatArray {
+        val result = tempResult
+        val startAngle = Math.PI * (9 / 8.0)   // Angles are in radians.
+        val angle = startAngle + pos * (Math.PI / 4)
+        result[0] = (radius * Math.cos(angle)).toFloat() + width / 2
+        result[1] = (radius * Math.sin(angle)).toFloat() + height / 2
+        return result
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        width = w.toFloat()
+        height = h.toFloat()
+        radius = (Math.min(width, height) / 2 * 0.8).toFloat()
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        // Draw the dial.
+        canvas.drawCircle(width / 2, height / 2, radius, dialPaint)
+        // Draw the text labels.
+        val labelRadius = radius + 20
+        val label = tempLabel
+        for (i in 0 until SELECTION_COUNT) {
+            val xyData = computeXYForPosition(i, labelRadius)
+            val x = xyData[0]
+            val y = xyData[1]
+            label.setLength(0)
+            label.append(i)
+            canvas.drawText(label, 0, label.length, x, y, textPaint)
+        }
+        // Draw the indicator mark.
+        val markerRadius = radius - 35
+        val xyData = computeXYForPosition(activeSelection,
+                markerRadius)
+        val x = xyData[0]
+        val y = xyData[1]
+        canvas.drawCircle(x, y, 20f, textPaint)
     }
 }
