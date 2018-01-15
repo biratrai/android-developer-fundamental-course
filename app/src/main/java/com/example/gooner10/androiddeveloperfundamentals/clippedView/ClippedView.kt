36abaeb6ceb2bcda1c170c5bd05f345318ab418f
Birat Rai <biratrai@hotmail.com>
2018-01-15 07:51:23 -0500

    adding paint strokewidth, textsize and setting RectF

--- app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/clippedView/ClippedView.kt
+++ app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/clippedView/ClippedView.kt
@@ -3,6 +3,7 @@ package com.example.gooner10.androiddeveloperfundamentals.clippedView
 import android.content.Context
 import android.graphics.Paint
 import android.graphics.Path
+import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
 import com.example.gooner10.androiddeveloperfundamentals.R
@@ -11,8 +12,8 @@ import com.example.gooner10.androiddeveloperfundamentals.R
  * Custom view to display clipped view
  */
 class ClippedView : View {
-    private var paint: Paint? = null
-    private var path: Path? = null
+    private var paint: Paint = Paint()
+    private var path: Path = Path()
     private val clipRectRight = resources.getDimension(R.dimen.clipRectRight)
     private val clipRectBottom = resources.getDimension(R.dimen.clipRectBottom)
     private val clipRectTop = resources.getDimension(R.dimen.clipRectTop)
@@ -22,9 +23,23 @@ class ClippedView : View {
     private val circleRadius = resources.getDimension(R.dimen.circleRadius)
     private val textOffset = resources.getDimension(R.dimen.textOffset)
     private val textSize = resources.getDimension(R.dimen.textSize)
+    private val columnOne = rectInsect
+    private val columnTwo = columnOne + rectInsect + clipRectRight
+    private val rowOne = rectInsect
+    private val rowTwo = rowOne + rectInsect + clipRectBottom
+    private val rowThree = rowTwo + rectInsect + clipRectBottom
+    private val rowFour = rowThree + rectInsect + clipRectBottom
+    private val textRow = rowFour + (1.5 * clipRectBottom)
+    private var rectF: RectF? = null
 
     constructor(context: Context?) : super(context)
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {
+        paint.isAntiAlias = true
+        paint.strokeWidth = resources.getDimension(R.dimen.strokeWidth)
+        paint.textSize = resources.getDimension(R.dimen.textSize)
+        rectF = RectF(RectF(rectInsect, rectInsect, clipRectRight - rectInsect, clipRectBottom - rectInsect))
+    }
+
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
 }
