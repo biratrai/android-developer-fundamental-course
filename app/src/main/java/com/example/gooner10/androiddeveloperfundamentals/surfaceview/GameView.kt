4b85200b27fee5b1ac9ac59225071071e4faabb6
Birat Rai <biratrai@hotmail.com>
2018-01-20 22:25:37 -0500

    adding init methods to initialize surfaceholder, paint, path

--- app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/surfaceview/GameView.kt
+++ app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/surfaceview/GameView.kt
@@ -1,14 +1,21 @@
 package com.example.gooner10.androiddeveloperfundamentals.surfaceview
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
+import android.view.SurfaceHolder
 import android.view.SurfaceView
 
 /**
  * Custom SurfaceView class
  */
 class GameView : SurfaceView, Runnable {
-    private var mContext: Context? = null
+    private lateinit var mContext: Context
+    private lateinit var surfaceHolder: SurfaceHolder
+    private lateinit var paint: Paint
+    private lateinit var path: Path
 
     constructor(context: Context) : super(context) {
         init(context)
@@ -22,9 +29,12 @@ class GameView : SurfaceView, Runnable {
         init(context)
     }
 
-
     private fun init(context: Context) {
         mContext = context
+        surfaceHolder = holder
+        paint = Paint()
+        paint.color = Color.DKGRAY
+        path = Path()
     }
 
     override fun run() {
