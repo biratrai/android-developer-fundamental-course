07fc89adb63d3e1f242abc299d8a9e5763f761b4
Birat Rai <biratrai@hotmail.com>
2018-01-21 11:15:23 -0500

    correcting the button name and adding setUpBitmap function

--- app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/surfaceview/GameView.kt
+++ app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/surfaceview/GameView.kt
@@ -1,9 +1,7 @@
 package com.example.gooner10.androiddeveloperfundamentals.surfaceview
 
 import android.content.Context
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Path
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.SurfaceHolder
 import android.view.SurfaceView
@@ -16,6 +14,12 @@ class GameView : SurfaceView, Runnable {
     private lateinit var surfaceHolder: SurfaceHolder
     private lateinit var paint: Paint
     private lateinit var path: Path
+    private var bitmapY: Int? = null
+    private var bitmapX: Int? = null
+    private lateinit var bitmap: Bitmap
+    private val viewWidth: Int? = null
+    private val viewHeight: Int? = null
+    private lateinit var winnerRect: RectF
 
     constructor(context: Context) : super(context) {
         init(context)
@@ -38,15 +42,17 @@ class GameView : SurfaceView, Runnable {
     }
 
     override fun run() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     fun pause() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     fun resume() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
+    fun setUpBitmap() {
+        bitmapX = Math.floor(Math.random() * (viewWidth!! - bitmap.width)).toInt()
+        bitmapY = Math.floor(Math.random() * (viewHeight!! - bitmap.height)).toInt()
+        winnerRect = RectF(bitmapX!!.toFloat(), bitmapY!!.toFloat(), (bitmapX!! + bitmap.width).toFloat(), (bitmapY!! + bitmap.height).toFloat())
+    }
 }
