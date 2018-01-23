53ae33fd189c92c52c33b64a1cfc7c998ab41d16
Birat Rai <biratrai@hotmail.com>
2018-01-22 20:58:55 -0500

    adding sestRadius method

--- app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/animations/PulseAnimationView.kt
+++ app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/animations/PulseAnimationView.kt
@@ -1,6 +1,8 @@
 package com.example.gooner10.androiddeveloperfundamentals.animations
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 
@@ -8,6 +10,16 @@ import android.view.View
  * Custom animation view
  */
 class PulseAnimationView : View {
+    private var radius: Float? = null
+    private val paint: Paint = Paint()
+    private val COLOR_ADJUSTER = 5
+
     constructor(context: Context?) : super(context)
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+
+    fun setRadius(radius: Float) {
+        this.radius = radius
+        paint.color = (Color.GREEN + radius / COLOR_ADJUSTER).toInt()
+        invalidate()
+    }
 }
