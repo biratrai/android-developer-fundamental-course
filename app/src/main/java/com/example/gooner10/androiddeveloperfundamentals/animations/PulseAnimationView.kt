6c8133893d6f4cab884b519ae808c017284e9903
Birat Rai <biratrai@hotmail.com>
2018-01-23 07:40:22 -0500

    adding onTouchEvent to find the x and y coordinates

--- app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/animations/PulseAnimationView.kt
+++ app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/animations/PulseAnimationView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 
 /**
@@ -13,13 +14,24 @@ class PulseAnimationView : View {
     private var radius: Float? = null
     private val paint: Paint = Paint()
     private val COLOR_ADJUSTER = 5
+    private var mX: Float? = null
+    private var mY: Float? = null
 
     constructor(context: Context?) : super(context)
+
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
 
     fun setRadius(radius: Float) {
         this.radius = radius
         paint.color = (Color.GREEN + radius / COLOR_ADJUSTER).toInt()
         invalidate()
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        if (event.actionMasked == MotionEvent.ACTION_DOWN) {
+            this.mX = event.x
+            this.mY = event.y
+        }
+        return super.onTouchEvent(event)
     }
 }
