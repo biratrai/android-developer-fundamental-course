26c3b76166c2ac80fd1e6509042e6076216507e5
Birat Rai <biratrai@hotmail.com>
2018-01-22 20:51:31 -0500

    adding custom animation view and activity

---
+++ app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/animations/PulseAnimationView.kt
@@ -0,0 +1,13 @@
+package com.example.gooner10.androiddeveloperfundamentals.animations
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+/**
+ * Custom animation view
+ */
+class PulseAnimationView : View {
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+}
