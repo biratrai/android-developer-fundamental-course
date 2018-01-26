eee21d59790d8a4c7a12bc0c80466e6a9f4d3a70
Birat Rai <biratrai@hotmail.com>
2018-01-25 22:37:02 -0500

    adding touch listener to actitivy

--- app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/physicsanimation/PhysicsAnimationActivity.kt
+++ app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/physicsanimation/PhysicsAnimationActivity.kt
@@ -1,13 +1,25 @@
 package com.example.gooner10.androiddeveloperfundamentals.physicsanimation
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
 import com.example.gooner10.androiddeveloperfundamentals.R
+import org.jetbrains.anko.contentView
 
-class PhysicsAnimationActivity : AppCompatActivity() {
+class PhysicsAnimationActivity : AppCompatActivity(), View.OnTouchListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_physics_animation)
+        contentView?.setOnTouchListener(this);
+    }
+
+    override fun onTouch(v: View?, event: MotionEvent): Boolean {
+        Log.d("TAG", "onTouch Clicked")
+
+        return super.onTouchEvent(event);
     }
 }
+
