6a5a4a120913e940efa02ed2468949c159a562dd
Birat Rai <biratrai@hotmail.com>
2018-01-25 22:28:21 -0500

    adding phsycis animation activity

---
+++ app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/physicsanimation/PhysicsAnimationActivity.kt
@@ -0,0 +1,13 @@
+package com.example.gooner10.androiddeveloperfundamentals.physicsanimation
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gooner10.androiddeveloperfundamentals.R
+
+class PhysicsAnimationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_physics_animation)
+    }
+}
