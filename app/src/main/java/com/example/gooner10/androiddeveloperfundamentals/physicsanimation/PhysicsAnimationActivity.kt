677cc3705e2db51c7ddfcf03a9036334d25333ee
Birat Rai <biratrai@hotmail.com>
2018-01-25 22:41:53 -0500

    adding fling animation

--- app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/physicsanimation/PhysicsAnimationActivity.kt
+++ app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/physicsanimation/PhysicsAnimationActivity.kt
@@ -1,25 +1,33 @@
 package com.example.gooner10.androiddeveloperfundamentals.physicsanimation
 
 import android.os.Bundle
+import android.support.animation.DynamicAnimation
+import android.support.animation.FlingAnimation
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import com.example.gooner10.androiddeveloperfundamentals.R
+import kotlinx.android.synthetic.main.activity_physics_animation.*
 import org.jetbrains.anko.contentView
+
 
 class PhysicsAnimationActivity : AppCompatActivity(), View.OnTouchListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_physics_animation)
-        contentView?.setOnTouchListener(this);
+        contentView?.setOnTouchListener(this)
+
     }
 
     override fun onTouch(v: View?, event: MotionEvent): Boolean {
         Log.d("TAG", "onTouch Clicked")
-
-        return super.onTouchEvent(event);
+        val flingAnimation = FlingAnimation(animationText, DynamicAnimation.X)
+        flingAnimation.setStartVelocity(500f)
+        flingAnimation.friction = 0.5f
+        flingAnimation.start()
+        return super.onTouchEvent(event)
     }
 }
 
