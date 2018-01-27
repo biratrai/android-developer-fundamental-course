938dad7e7d605e8163d735a0592e9b48bab3548e
Birat Rai <biratrai@hotmail.com>
2018-01-26 20:42:00 -0500

    adding spring animation

--- app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/physicsanimation/PhysicsAnimationActivity.kt
+++ app/src/main/java/com/example/gooner10/androiddeveloperfundamentals/physicsanimation/PhysicsAnimationActivity.kt
@@ -3,6 +3,8 @@ package com.example.gooner10.androiddeveloperfundamentals.physicsanimation
 import android.os.Bundle
 import android.support.animation.DynamicAnimation
 import android.support.animation.FlingAnimation
+import android.support.animation.SpringAnimation
+import android.support.animation.SpringForce
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.MotionEvent
@@ -27,7 +29,19 @@ class PhysicsAnimationActivity : AppCompatActivity(), View.OnTouchListener {
         flingAnimation.setStartVelocity(500f)
         flingAnimation.friction = 0.5f
         flingAnimation.start()
+        springAnimation()
         return super.onTouchEvent(event)
+    }
+
+    private fun springAnimation() {
+        val springAnimation = SpringAnimation(animationText, DynamicAnimation.X)
+        val springForce = SpringForce()
+        springForce.finalPosition = animationText.x
+        springForce.dampingRatio = SpringForce.DAMPING_RATIO_HIGH_BOUNCY
+        springForce.stiffness = SpringForce.STIFFNESS_LOW
+        springAnimation.spring = springForce
+        springAnimation.setStartVelocity(2000f)
+        springAnimation.start()
     }
 }
 
